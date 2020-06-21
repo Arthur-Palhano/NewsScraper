a8d8be72150e1f6a24d7aa5a97fd59955850fdbc
Arthur Palhano <pauloart.hur670@gmail.com>
2020-06-21 20:36:40 -0300

    Adicionando MSNNews

--- news.py
+++ news.py
@@ -4,18 +4,39 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.headless = True
 
-firefox = webdriver.Firefox(options=options)
-firefox.get("https://www.msn.com/pt-br/")
-
-bruteNews = firefox.find_elements_by_class_name("title")
-
 news = []
 
-for new in bruteNews:
-    if new.text != "":
-        news.append(new.text)
+def MSN():
+  firefox = webdriver.Firefox(options=options)
+  firefox.get("https://www.msn.com/pt-br/")
 
-firefox.close()
+  bruteNews = firefox.find_elements_by_class_name("title")
+
+  news = []
+
+  for new in bruteNews:
+      if new.text != "":
+          news.append(new.text)
+  firefox.close()
+
+###--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------###
+
+def MSNNews():
+  firefox = webdriver.Firefox(options=options)
+  firefox.get("https://www.msn.com/pt-br/noticias")
+
+  bruteNews = firefox.find_elements_by_tag_name("h3")
+
+  for new in bruteNews:
+      if new.text != "":
+          news.append(new.text)
+  firefox.close()
+
+def search():
+  MSN()
+  MSNNews()
+
+search()
 
 for new in news:
     print(f"° {new}")
